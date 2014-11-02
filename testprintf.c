375c41d6b3534b4296c87ea91e95fe705eab2810
ranlei <ranleixxx@gmail.com>
2014-11-02 22:07:16 +0800

    modify

--- testprintf.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    struct x {int a;int b;char c;};
-    struct y {int a;char c;int b;char m;};
-    printf("x=%d\n",sizeof(struct x));
-    printf("y=%d\n",sizeof(struct y));
-}
